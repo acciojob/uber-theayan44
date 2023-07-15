079be8092b6c488543abbcdc8dde42bfd311ce9b
ayan <theayan44@gmail.com>
2023-07-15 15:29:22 +0530

    Code Done

--- src/main/java/com/driver/controllers/CustomerController.java
+++ src/main/java/com/driver/controllers/CustomerController.java
@@ -29,7 +29,10 @@ public class CustomerController {
 	@PostMapping("/bookTrip")
 	public ResponseEntity<Integer> bookTrip(@RequestParam Integer customerId, @RequestParam String fromLocation, @RequestParam String toLocation, @RequestParam Integer distanceInKm) throws Exception {
 		TripBooking bookedTrip = customerService.bookTrip(customerId, fromLocation, toLocation, distanceInKm);
-		return new ResponseEntity<>(bookedTrip.getTripBookingId(), HttpStatus.CREATED);
+		if(bookedTrip != null)
+			return new ResponseEntity<>(bookedTrip.getTripBookingId(), HttpStatus.CREATED);
+		else
+			return new ResponseEntity<>(0, HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/complete")
